e4550e5a0b4d10065fd750ea4b682f6f5d75f539
SocioPath101 <56000035+SocioPath101@users.noreply.github.com>
2020-10-06 23:04:35 +0530

    Update models.py

--- Kaustubh/Play/models.py
+++ Kaustubh/Play/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 class Album(models.Model):
 	artist = models.CharField(max_length=250)
-	album_title= models.CharField(max_length=500)
+	album_title= models.CharField(max_length=250)
 	genre= models.CharField(max_length=100)
-	album_logo = models.CharField(max_length=1000)
+	album_logo = models.CharField(max_length=100)
 
 
 
